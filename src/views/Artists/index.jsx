a038de1800d758317af89ff4ccb273fd749be5d2
Alejandro <aalejo87@gmail.com>
2020-10-07 19:30:47 -0500

    PLFM-4 Fixing fetch
    - Build works on Android still no fetch functionality

--- src/views/Artists/index.jsx
+++ src/views/Artists/index.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Text } from 'react-native';
 import ScrollContainer from '../../common/components/ScrollContainer';
-import Fetcher from '../../common/components/Fetcher';
 
 const Artists = () => {
   return (
     <ScrollContainer>
-      <Text>
-        <Fetcher apiKey="829751643419a7128b7ada50de590067" />
-      </Text>
+      <Text>OLI 1</Text>
     </ScrollContainer>
   );
 };
