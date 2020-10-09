fb99582baa7e247436b646fa79fdcc64c103fec9
Alejandro <aalejo87@gmail.com>
2020-10-08 19:56:28 -0500

    PLFM-8 Fixed styles and display
    - Fixed styles and display methods

--- src/common/components/Card/index.jsx
+++ src/common/components/Card/index.jsx
@@ -7,9 +7,9 @@ const Card = ({ item }) => {
   return (
     <View style={style.container}>
       <Text style={style.artist}>Artista: {item.name}</Text>
-      <Text style={style.name}>Reproducciones: {item.listeners}</Text>
+      <Text style={style.listeners}>Reproducciones: {item.listeners}</Text>
       <Image style={style.image} source={{ uri: item.image[2]['#text'] }} />
-      <Text style={style.description}>{item.url}</Text>
+      <Text style={style.url}>{item.url}</Text>
     </View>
   );
 };
